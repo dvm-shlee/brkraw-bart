51f41126ee87d7ad9c9bba8f7b04e2f06c27e298
SungHo Lee <shlee@unc.edu>
2022-10-03 15:47:50 -0400

    Update __init__.py
    version 0.0.1 to 0.0.2

--- brkbart/__init__.py
+++ brkbart/__init__.py
@@ -11,7 +11,7 @@ import tempfile as tmp
 import paralexe as pe
 import multiprocessing as mp
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 try:
     __toolbox_path = config.config.get('Default', 'toolbox_path')
